4ae7f60e97ffb29024f954397f38ae4c83b6d0b2
Reda <reda.bouchaala@printify.com>
2024-12-13 17:50:24 +0100

    Handle non-array param to PHP Redis::set call
    Signed-off-by: Reda <reda.bouchaala@printify.com>

--- src/RedisInstrumentation.php
+++ src/RedisInstrumentation.php
@@ -320,6 +320,12 @@ class RedisInstrumentation
                 if ($class === \Redis::class) {
                     $statement = 'SET ' . $params[0] . ' ?';
                     if (isset($params[2])) {
+                        // Third param could be the expiry time
+                        // @see: https://github.com/phpredis/phpredis?tab=readme-ov-file#parameters-23
+                        if (is_int($params[2])) {
+                            $params[2] = ['EX' => $params[2]];
+                        }
+
                         foreach ($params[2] as $key => $value) {
                             $statement .= ' ' . strtoupper((string) $key) . ' ' . $value;
                         }
